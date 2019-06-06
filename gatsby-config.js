c02f4ea2a2720f9bbccad3e3b292a0ad19b54cd2
fune0 <fune0x8@gmail.com>
2019-06-06 11:18:16 +0900

    ページタイトル修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio Site`,
+    description: `Hello, SHIBUYA. I'm Fune0. I live in Shibuya ward to enjoy solitude. But I sometimes feel lonely.`,
+    author: `@funeo_x8`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
